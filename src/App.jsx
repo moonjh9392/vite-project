76ad5952d8d5ebb191cc87e0c23bcc3199672e0c
moonjh9392 <moonjh9392@gmail.com>
2024-04-15 17:45:23 +0900

    v2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
+import { BrowserRouter } from 'react-router-dom';
 import Router from './router/Router';
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Router />
-    </>
+    </BrowserRouter>
   );
 }
 
--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -1,10 +1,11 @@
 import { Suspense, lazy } from 'react';
 import { useRoutes } from 'react-router-dom';
 
-const Home = lazy(() => import('./home'));
+const Home = lazy(() => import('../pages/Home/Home'));
 
 const Router = () => {
   const routes = useRoutes([{ path: '/', element: <Home /> }]);
+
   return <Suspense fallback={<div>Loading...</div>}>{routes}</Suspense>;
 };
 
