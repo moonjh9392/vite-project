1833a5ad2768cf9cd200d0e040a725c5f00cec87
moonjh9392 <moonjh9392@gmail.com>
2024-04-15 18:04:32 +0900

    v3

---
+++ src/assets/svg/index.js
@@ -0,0 +1,18 @@
+// export const BracketU24Icon = () => {
+//   return (
+//     <svg
+//       width="24"
+//       height="24"
+//       viewBox="0 0 24 24"
+//       fill="none"
+//       xmlns="http://www.w3.org/2000/svg">
+//       <path
+//         d="M18 15.5L12 9.5L6 15.5"
+//         stroke="#222222"
+//         strokeWidth="1.5"
+//         strokeLinecap="round"
+//         strokeLinejoin="round"
+//       />
+//     </svg>
+//   );
+// };
